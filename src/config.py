a3acea67e1239ab3e423f83f47efbc73c8776f4f
André Moreira <andmoreira98@gmail.com>
2025-04-23 19:36:38 -0300

    Added new directories in `config.py` for managing ML runs and reports.

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 CONFIG_DIR = PROJ_ROOT / "configs"
+
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
@@ -19,7 +20,10 @@ EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
 MODELS_DIR = PROJ_ROOT / "models"
 
+MLRUNS_DIR = PROJ_ROOT / "mlruns"
+
 REPORTS_DIR = PROJ_ROOT / "reports"
+
 FIGURES_DIR = REPORTS_DIR / "figures"
 
 # If tqdm is installed, configure loguru with tqdm.write
