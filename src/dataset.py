39e1369f2378fc9987732ae4c97a6a1f59c7c407
André Moreira <andmoreira98@gmail.com>
2025-07-15 22:14:06 -0300

    Feature selection removed.

--- src/dataset.py
+++ src/dataset.py
@@ -25,7 +25,7 @@ def main(
     # ----  DEFAULT PATHS --------------------------
     asset: str = '^BVSP',
     asset_focus: str = 'Close',
-    years: int = 3
+    years: int = 10
     # ----------------------------------------------
 ):
     # -----------------------------------------
@@ -64,7 +64,7 @@ def main(
     
     df_raw = pd.read_csv(RAW_DATA_DIR / 'dataset.csv', index_col=0) # ensure coupling
 
-    print(df_raw.tail(7))
+    print(df_raw.tail(3))
 
     logger.success("Raw data successfully loaded...")
 
@@ -77,33 +77,35 @@ def main(
     df_raw = df_raw.pct_change(periods=1,fill_method=None)
     df_raw = df_raw.replace([np.inf, -np.inf], 0).fillna(0)
     
-    print(df_raw.tail(7))
+    print(df_raw.tail(3))
 
+    '''
     y = df_raw[target_col]  # This column is kept only for cleaning purposes.
     X = df_raw.drop(columns=target_col)
-
+   
     clean_pipeline = CleanPipeline([
-            #CleanMissingValues(),
-            #CleanLowVariance(),
-            #CleanGenericUnivariate(f_classif, "percentile", 95),    
-            CleanGenericUnivariate(f_regression, "percentile", 95)
+            CleanMissingValues(),
+            CleanLowVariance(),
+            CleanGenericUnivariate(f_classif, "percentile", 99),    
+            CleanGenericUnivariate(f_regression, "percentile", 99)
             ])
 
     X_clean, y_clean = clean_pipeline.clear(X, y)
 
     df_clean = pd.concat([y_clean,X_clean],axis=1)
-
+    
     dataset_path = PROCESSED_DATA_DIR / 'dataset.csv'
-
+    
     df_old_dataset = pd.read_csv(dataset_path, index_col=0) if dataset_path.exists() else df_clean.copy()
 
     print(df_old_dataset.tail(7))
 
     df_updated = update_df(df_old_dataset, df_clean)
+    '''
 
-    df_dataset = enrich_calendar(df_updated)
+    df_dataset = enrich_calendar(df_raw)
     
-    print(df_dataset.tail(7))
+    print(df_dataset.tail(3))
 
     df_dataset.to_csv(PROCESSED_DATA_DIR / 'dataset.csv')
     logger.success("Clean data successfully loaded...")
