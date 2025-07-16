4ad271b8da96f679d4ad3fab680955deae5de72f
André Moreira <andmoreira98@gmail.com>
2025-07-15 22:17:36 -0300

    Add parameter to tune.

--- src/utils/tune/tuner_builder.py
+++ src/utils/tune/tuner_builder.py
@@ -3,6 +3,7 @@ from src.config import MODELS_DIR, logger
 
 import keras_tuner as kt
 from keras_tuner import HyperParameters
+from tensorflow.keras.losses import Huber
 
 from src.utils.train.model_builder import RegressionRobustModelBuilder
 from src.utils.train.compile_strategy import ICompileStrategy,RegressionCompileStrategy
@@ -19,31 +20,40 @@ class ITunerBuilder(ABC):
 
 class RegressionRobustModelTuner(ITunerBuilder):
 
-    def __init__(self, input_shape, output_shape,max_trials : int =10, project_name : str = "default", compile_strategy = None):
+    def __init__(self, input_shape, output_shape,max_trials : int =10, project_name : str = "default_tune"):
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.max_trials = max_trials
         self.project_name = project_name
-        self.compile_strategy = compile_strategy or RegressionCompileStrategy()
+        self.compile_strategy = None
 
     def _build_model(self,hp: HyperParameters):
 
-        l2_rate = hp.Float("l2_rate", min_value=1e-4, max_value=1e-3, step=1e-4)
-        dropout_rate= hp.Float("dropout_rate", min_value=0.1, max_value=0.5, step=0.1)
+        delta_rate = hp.Float("delta_rate", min_value=0.8, max_value=3.0, step=0.1)
+        self.compile_strategy= RegressionCompileStrategy(loss = Huber(delta=delta_rate))
+
+        kernel_rate= hp.Int("kernel_rate", min_value=3, max_value=7, step=1)
+        l2_rate = hp.Float("l2_rate", min_value=1e-5, max_value=1e-2, sampling="log")
+        dropout_rate= hp.Float("dropout_rate", min_value=0.1, max_value=0.5)
         conv1D_units = hp.Int("conv1D_units", min_value=64, max_value=256, step=32)
-        lstm_units = hp.Int("lstm_units", min_value=32, max_value=128, step=16)
-        dense_units = hp.Int("dense_units", min_value=32, max_value=128, step=16)
-        gru_units = hp.Int("gru_units", min_value=32, max_value=128, step=16) 
+        gru_units = hp.Int("gru_units", min_value=32, max_value=128, step=8)
+        mid_dense_units = hp.Int("mid_dense_units", min_value=32, max_value=128, step=16)
+        negative_slope_rate = hp.Float("alpha_rate", min_value=0.01, max_value=0.2, step=0.01)
+        lstm_units = hp.Int("lstm_units", min_value=32, max_value=128, step=8)
+        low_dense_units = hp.Int("low_dense_units", min_value=32, max_value=128, step=16)
 
         builder = RegressionRobustModelBuilder(
             input_shape = self.input_shape,
             output_shape = self.output_shape,
+            kernel_rate = kernel_rate,
             l2_rate = l2_rate,
             dropout_rate = dropout_rate,
             conv1D_units = conv1D_units,
+            gru_units = gru_units,
+            mid_dense_units = mid_dense_units,
+            negative_slope_rate = negative_slope_rate,
             lstm_units = lstm_units,
-            dense_units = dense_units,
-            gru_units = gru_units
+            low_dense_units = low_dense_units
         )
 
         model =  builder.build_model()
